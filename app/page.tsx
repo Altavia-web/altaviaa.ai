3b61f877252e84ae9d3d8503b4e05853489e1013
joka21 <jo.kalenberb@web.de>
2025-09-29 16:03:12 +0200

    section löschen

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,6 @@ import Footer from "@/components/Footer";
 import HomeHero from "@/components/sections/home/home-hero";
 import HomeMarketOptimization from "@/components/sections/home/home-market-optimization";
 import HomeCompany from "@/components/sections/home/home-company";
-import HomeLocalization from "@/components/sections/home/home-localization";
-import HomeDatev from "@/components/sections/home/home-datev";
-import HomeCompliance from "@/components/sections/home/home-compliance";
-import HomePartners from "@/components/sections/home/home-partners";
-import HomeCta from "@/components/sections/home/home-cta";
 
 export default function Home() {
   return (
@@ -14,11 +9,6 @@ export default function Home() {
       <HomeHero />
       <HomeMarketOptimization />
       <HomeCompany />
-      <HomeLocalization />
-      <HomeDatev />
-      <HomeCompliance />
-      <HomePartners />
-      <HomeCta />
       <Footer />
     </div>
   );
