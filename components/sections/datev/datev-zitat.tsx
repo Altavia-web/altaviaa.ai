dac9aa5782137a03ecdf77db6d654c849df52f7e
joka21 <jo.kalenberb@web.de>
2025-10-01 17:02:31 +0200

    bg

--- components/sections/datev/datev-zitat.tsx
+++ components/sections/datev/datev-zitat.tsx
@@ -6,7 +6,7 @@ export default function DatevZitat() {
       className="relative flex items-center justify-center py-16"
       style={{
         minHeight: '72vh',
-        background: 'var(--gradient-datev)'
+        background: 'var(--gradient-datev-intro)'
       }}
     >
       {/* Content */}
