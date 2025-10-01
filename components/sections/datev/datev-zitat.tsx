d31da13ba1e7606fc88b3a651e74b599d9a1679d
joka21 <jo.kalenberb@web.de>
2025-10-01 17:08:53 +0200

    zitatbild

--- components/sections/datev/datev-zitat.tsx
+++ components/sections/datev/datev-zitat.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function DatevZitat() {
   return (
@@ -9,6 +10,26 @@ export default function DatevZitat() {
         background: 'var(--gradient-datev-intro)'
       }}
     >
+      {/* Opening Quote Icon - Top Left */}
+      <div
+        className="absolute"
+        style={{
+          top: '84.21px',
+          left: '675.27px',
+          width: '89.46px',
+          height: '77.87px',
+          opacity: 1
+        }}
+      >
+        <Image
+          src="/images/zitatBlau.png"
+          alt="Quote"
+          width={90}
+          height={78}
+          style={{ width: '100%', height: '100%' }}
+        />
+      </div>
+
       {/* Content */}
       <div className="relative z-10 mx-auto px-6 text-center" style={{ width: '80%' }}>
         {/* Zitat Text - Placeholder */}
@@ -19,7 +40,7 @@ export default function DatevZitat() {
             fontSize: '28px',
             lineHeight: '140%',
             letterSpacing: '0',
-            color: '#ffffff'
+            color: '#002E64'
           }}
         >
           [Zitat Text folgt]
