af2992237c3a8c04ebfa443a3c165e24d08224ab
joka21 <jo.kalenberb@web.de>
2025-10-01 15:55:04 +0200

    test

--- components/sections/datev/datev-schnittstelle.tsx
+++ components/sections/datev/datev-schnittstelle.tsx
@@ -15,38 +15,36 @@ export default function DatevSchnittstelle() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Text Content - Left */}
           <div className="text-left">
-            <h1
+            <h3
               className="mb-6"
               style={{
                 fontFamily: 'Titillium Web',
                 fontWeight: 700,
-                fontSize: '69px',
+                fontSize: '44px',
                 lineHeight: '110%',
                 letterSpacing: '0',
-                color: '#002e64'
+                color: '#002E64'
               }}
             >
-              DATEV-Schnittstelle
-            </h1>
-            <h2
-              style={{
-                fontFamily: 'Titillium Web',
-                fontWeight: 700,
-                fontSize: '55px',
-                lineHeight: '110%',
-                letterSpacing: '0',
-                color: '#002e64'
-              }}
-            >
-              [H2 Text folgt]
-            </h2>
+              [H3 Text folgt - kann mehrzeilig sein]
+            </h3>
+            <p style={{
+              fontFamily: 'Titillium Web',
+              fontWeight: 600,
+              fontSize: '24px',
+              lineHeight: '140%',
+              letterSpacing: '0',
+              color: '#000000'
+            }}>
+              Wenn Ihr Unternehmen als deutsche Tochtergesellschaft umfangreiche Finanzberichtspflichten in Deutschland hat und DATEV Unternehmen online für die Buchhaltung nutzt, ist DATEV Export für NetSuite die richtige Wahl.
+            </p>
           </div>
 
           {/* Image - Right */}
           <div className="flex justify-end">
             <div className="relative w-full max-w-lg">
               <Image
-                src="/images/DATEV/[Bildname folgt].webp"
+                src="/images/produkte/AVA_DATEV_DE.webp"
                 alt="DATEV Schnittstelle"
                 width={500}
                 height={500}
