d1acf2e0e6a09e02cf05d81a95440f43898ec34b
joka21 <jo.kalenberb@web.de>
2025-10-01 11:29:35 +0200

    test

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '@/components/Footer';
+import ProductsHero from '@/components/sections/products/products-hero';
+
+export default function Products() {
+  return (
+    <div className="min-h-screen">
+      <ProductsHero />
+      <Footer />
+    </div>
+  );
+}
