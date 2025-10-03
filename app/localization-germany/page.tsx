b3b80fbb3911fa38361262b8c034896ae742b9a1
joka21 <jo.kalenberb@web.de>
2025-10-03 11:42:26 +0200

    update

--- app/localization-germany/page.tsx
+++ app/localization-germany/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Footer from '@/components/Footer';
 import LocalizationGermanyHero from '@/components/sections/localization-germany/localization-germany-hero';
+import LocalizationHero from '@/components/sections/localization/LocalizationHero';
+import LocalizationVideo from '@/components/sections/localization/LocalizationVideo';
 
 export default function LocalizationGermany() {
   return (
     <div className="min-h-screen">
       <LocalizationGermanyHero />
+      <LocalizationHero />
+      <LocalizationVideo />
       <Footer />
     </div>
   );
