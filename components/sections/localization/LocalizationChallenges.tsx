502b6d83eb74bade38b5e1ddc08eddc9c0d8c4e2
joka21 <jo.kalenberb@web.de>
2025-10-03 12:06:42 +0200

    error

--- components/sections/localization/LocalizationChallenges.tsx
+++ components/sections/localization/LocalizationChallenges.tsx
@@ -52,11 +52,15 @@ export default function LocalizationChallenges() {
   const [touchEnd, setTouchEnd] = useState(0);
 
   const handleTouchStart = (e: React.TouchEvent) => {
-    setTouchStart(e.targetTouches[0].clientX);
+    if (e.targetTouches[0]) {
+      setTouchStart(e.targetTouches[0].clientX);
+    }
   };
 
   const handleTouchMove = (e: React.TouchEvent) => {
-    setTouchEnd(e.targetTouches[0].clientX);
+    if (e.targetTouches[0]) {
+      setTouchEnd(e.targetTouches[0].clientX);
+    }
   };
 
   const handleTouchEnd = () => {
